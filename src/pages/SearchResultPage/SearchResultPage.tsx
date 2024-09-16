4f57e2ef245b518ebdd5fe75e7fe0e1936fb179b
alinaguseva1997 <alinagorbunova93@gmail.com>
2024-09-17 02:04:13 +0300

    fix

--- src/pages/SearchResultPage/SearchResultPage.tsx
+++ src/pages/SearchResultPage/SearchResultPage.tsx
@@ -31,7 +31,7 @@ export const SearchResultPage = () => {
   return (
       <div className='pt-[12px]'>
         <div>
-          <div className='absolute left-[18%] top-[9%]'>
+          <div className='absolute left-[18%] top-[10%]'>
             <Toggle title='Искать по точному совпадению' checked={isChecked} onChange={() => {
             }}/>
           </div>
