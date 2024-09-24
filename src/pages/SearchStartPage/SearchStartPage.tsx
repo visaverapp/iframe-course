c9b2607730bdc9e0cc94fa332b6c24e832334af7
alinaguseva1997 <alinagorbunova93@gmail.com>
2024-09-24 10:14:22 +0300

    fix integration bug

--- src/pages/SearchStartPage/SearchStartPage.tsx
+++ src/pages/SearchStartPage/SearchStartPage.tsx
@@ -5,13 +5,13 @@ const SearchStartPage = () => {
 
   const startSearchPageSettings = {
     inputWidth: 'w-[706px]',
-    suggetionsPosition: 'top-[29%] w-[706px]',
+    suggetionsPosition: 'top-[27%] w-[706px]',
     navigatePath: '/search'
   }
 
   return (
-      <div className='mt-[15%] w-[1000px]'>
-        <div className='mb-[20%]'>
+      <div className='mt-[10%] w-[1000px]'>
+        <div className='mb-[33%]'>
           <SearchInput startSearchPageSettings={startSearchPageSettings}/>
         </div>
         <div className='flex justify-end'>
